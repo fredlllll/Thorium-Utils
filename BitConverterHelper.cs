7f39943ed9f5aa08e3daf7b44b42e753dbdc6cad
Frederik Gelder <frederik.gelder@freenet.de>
2018-08-20 14:10:03 +0200

    adding bitconverter helper

---
+++ BitConverterHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thorium.Utils
+{
+    public static class BitConverterHelper
+    {
+        public static byte[] GetBytesBigEndian(uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if(BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+    }
+}
