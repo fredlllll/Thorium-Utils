8cb122383bebb450e1ff450df9384c1cbd98b618
Frederik Gelder <frederik.gelder@freenet.de>
2018-08-20 14:09:52 +0200

    adding class for rolling over number sequence provider

---
+++ RollingOverUInt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thorium.Utils
+{
+    public class RollingOverUInt
+    {
+        readonly object nextLock = new object();
+
+        uint current = 0;
+        public uint Next
+        {
+            get
+            {
+                uint retval;
+                lock(nextLock)
+                {
+                    retval = current;
+                    if(current == uint.MaxValue)
+                    {
+                        current = uint.MinValue;
+                    }
+                    else
+                    {
+                        current++;
+                    }
+                }
+                return retval;
+            }
+        }
+    }
+}
